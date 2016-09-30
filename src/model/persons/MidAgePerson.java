0cbd25229600b2574673c9d4788e8b4b4e1a0110
Raphael Suter <raphael.suter@students.bfh.ch>
2016-09-30 15:41:23 +0200

    create currentPosition

--- src/model/persons/MidAgePerson.java
+++ src/model/persons/MidAgePerson.java
@@ -2,16 +2,18 @@ package model.persons;
 
 import java.util.Random;
 
+import model.Position;
+
 /**
  * Created by tgdflto1 on 30/09/16.
  */
-public class MidAgePerson extends Person{
-    public MidAgePerson(double maxHeigth, double maxWidth){
+public class MidAgePerson extends Person {
+	public MidAgePerson(double maxHeigth, double maxWidth) {
 
-        Random r = new Random();
-        double randomWidth = 0 + (maxWidth - PERSON_RADIUS) * r.nextDouble();
-        double randomHeight = 0 + (maxHeigth - PERSON_RADIUS) * r.nextDouble();
-
-        this.relocate(randomWidth, randomHeight);
-    }
+		Random r = new Random();
+		double randomWidth = 0 + (maxWidth - PERSON_RADIUS) * r.nextDouble();
+		double randomHeight = 0 + (maxHeigth - PERSON_RADIUS) * r.nextDouble();
+		this.setCurrentPosition(new Position(randomWidth, randomHeight));
+		this.relocate(randomWidth, randomHeight);
+	}
 }
