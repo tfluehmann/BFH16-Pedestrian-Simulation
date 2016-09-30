f622e7d7cb878fd8d9b0a7349de6bb3d2027be36
Flühmann Tobias, INI-ON-NTO-COY-NNI <tobias.fluehmann@swisscom.com>
2016-09-30 15:35:45 +0200

    constants for sizes

--- src/model/Room.java
+++ src/model/Room.java
@@ -20,18 +20,26 @@ public class Room extends Pane {
 	private ArrayList<Area> goalAreas;
 	private ArrayList<Area> spawnAreas;
 
+	public static final int ROOM_HEIGHT = 400;
+	public static final int ROOM_WIDTH = 400;
+
+	public static final int GOAL_HEIGHT = 20;
+	public static final int GOAL_WIDTH = 50;
+
+	public static final int SPAWN_HEIGHT = 150;
+	public static final int SPAWN_WIDTH = 150;
 	public Room() {
 		super();
 		this.setStyle("-fx-background-color: gray;");
 
-		this.setPrefSize(400, 400);
-		SpawnArea sa = new SpawnArea(200.0, 200.0, new Position(0.0, 0.0));
-		GoalArea ga = new GoalArea(50.0, 20.0, new Position(350.0, 380.0));
+		this.setPrefSize(ROOM_WIDTH, ROOM_HEIGHT);
+		SpawnArea sa = new SpawnArea(SPAWN_WIDTH, SPAWN_HEIGHT, new Position(0.0, 0.0));
+		GoalArea ga = new GoalArea(GOAL_WIDTH, GOAL_HEIGHT, new Position(350.0, 380.0));
 
 		this.getChildren().add(sa);
 		this.getChildren().add(ga);
 		for (int i = 0; i < 5; i++)
-			persons.add(new MidAgePerson(200, 200));
+			persons.add(new MidAgePerson(SPAWN_WIDTH, SPAWN_HEIGHT));
 
 		this.getChildren().addAll(persons);
 
