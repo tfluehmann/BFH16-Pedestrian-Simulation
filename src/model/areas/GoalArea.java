d1f93a0667e2b8267ea4d7f60898e4ee46f0284a
Raphael Suter <raphael.suter@students.bfh.ch>
2016-09-30 15:29:51 +0200

    better goal point

--- src/model/areas/GoalArea.java
+++ src/model/areas/GoalArea.java
@@ -1,31 +1,30 @@
 package model.areas;
 
-import model.Position;
+import java.util.List;
 
 import javafx.scene.paint.Color;
-
-import java.util.List;
+import model.Position;
 
 /**
  * Created by tgdflto1 on 30/09/16.
  */
 public class GoalArea extends Area {
-    private double height;
-    private double width;
-    private Position position;
+	private double height;
+	private double width;
+	private Position position;
 
 	public GoalArea(double width, double height, Position position) {
-        this.position = position;
-        this.height = height;
-        this.width = width;
+		this.position = position;
+		this.height = height;
+		this.width = width;
 		this.relocate(position.getX(), position.getY());
 		this.setFill(Color.GREEN);
 		this.getPoints().addAll(new Double[] { 0.0, 0.0, width, 0.0, width, height, 0.0, height });
 	}
 
-    @Override
-    public List<Position> getEdges() {
-        edges.add(new Position(this.width, this.height));
-        return edges;
-    }
+	@Override
+	public List<Position> getEdges() {
+		edges.add(new Position(this.position.getX() + this.width / 2, this.position.getY() + this.height / 2));
+		return edges;
+	}
 }
