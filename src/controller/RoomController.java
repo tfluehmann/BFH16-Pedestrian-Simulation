fcd76d23619d157559a90e0249170dc6d54d703e
Flühmann Tobias, INI-ON-NTO-COY-NNI <tobias.fluehmann@swisscom.com>
2016-09-30 11:17:09 +0200

    initializable

--- src/controller/RoomController.java
+++ src/controller/RoomController.java
@@ -1,7 +1,21 @@
 package controller;
 
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by tgdflto1 on 30/09/16.
  */
-public class RoomController {
+public class RoomController implements Initializable {
+
+    @Override // This method is called by the FXMLLoader when initialization is complete
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+        System.out.print("hell yeah");
+
+        // initialize your logic here: all @FXML variables will have been injected
+
+    }
+
 }
