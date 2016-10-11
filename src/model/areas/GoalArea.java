906d7811ce266080e28808e4dcd9ecafd86e9797
Flühmann Tobias, INI-ON-NTO-COY-NNI <tobias.fluehmann@swisscom.com>
2016-10-11 18:45:20 +0200

    Corners have its own list

--- src/model/areas/GoalArea.java
+++ src/model/areas/GoalArea.java
@@ -3,6 +3,7 @@ package model.areas;
 import javafx.scene.paint.Color;
 import model.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,21 +13,19 @@ public class GoalArea extends Area {
 	private double height;
 	private double width;
 
-	private Position position;
-
 	public GoalArea(double width, double height, Position position) {
+		super(0.0, 0.0, width, 0.0, width, height, 0.0, height);
 		this.position = position;
 		this.height = height;
 		this.width = width;
 		this.relocate(position.getXValue(), position.getYValue());
 		this.setFill(Color.GREEN);
-		this.getPoints().addAll(new Double[] { 0.0, 0.0, width, 0.0, width, height, 0.0, height });
+
 	}
 
 	public List<Position> getCorners() {
-		edges.add(new Position(this.position.getXValue() + this.width / 2, this.position.getYValue() + this.height / 2));
-		return edges;
+		List<Position> l = new ArrayList<>();
+		l.add(new Position(this.position.getXValue() + this.width / 2, this.position.getYValue() + this.height / 2));
+		return l;
 	}
-
-
 }
