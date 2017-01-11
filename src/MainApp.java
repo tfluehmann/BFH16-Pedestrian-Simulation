1ea6a4506138868d32eab0af4d843cc56c8926bb
Raphael Suter <raphael.suter@students.bfh.ch>
2017-01-11 21:11:26 +0100

    added Logo and favicon

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,10 +1,12 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -26,11 +28,14 @@ public class MainApp extends Application {
 			// creating view for the initial configuration
 			FXMLLoader configLoader = new FXMLLoader();
 			configLoader.setLocation(MainApp.class.getResource("view/ConfigWindow.fxml"));
+			InputStream iconPath = getClass().getResourceAsStream("img/pedestrianSim_icon.jpg");
+			Image favicon = new Image(iconPath);
 			configScene = new Scene(configLoader.load());
 			configStage = new Stage();
 			configStage.setScene(configScene);
 			configStage.initModality(Modality.APPLICATION_MODAL);
 			configStage.setTitle("Configuration");
+			configStage.getIcons().add(favicon);
 			configStage.showAndWait();
 
 			// Creating view for the simulation
@@ -40,6 +45,7 @@ public class MainApp extends Application {
 			scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
             primaryStage.setTitle("Pedestrian Simulation");
+			primaryStage.getIcons().add(favicon);
 			primaryStage.show();
         } catch (Exception ex) {
 			Logger.getLogger(MainApp.class.getName()).log(Level.SEVERE, null, ex);
