a28d777fe3f8066591630c689cd2590164b8341e
Flühmann Tobias, INI-ON-NTO-COY-NNI <tobias.fluehmann@swisscom.com>
2017-01-18 20:34:00 +0100

    simplified class initialization

--- src/manager/SpawnManager.java
+++ src/manager/SpawnManager.java
@@ -6,7 +6,7 @@ import model.Position;
 import model.TargetVertex;
 import model.Vertex;
 import model.areas.SpawnArea;
-import model.persons.Person;
+import model.persons.*;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -19,130 +19,127 @@ import java.util.concurrent.ThreadLocalRandom;
  * The spawnmanager handles the initial placement of the persons
  */
 public class SpawnManager {
-	private static SpawnManager instance = null;
-	private final Vector<Person> passivePersons = new Vector<>();
-	private SpawnAreaManager spawnAreaManager = SpawnAreaManager.getInstance();
-	private ConfigModel config = ConfigModel.getInstance();
-	private Random rnd = new Random();
-	private Vector<Person> persons = new Vector<>();
-	private PathManager pathManager = new PathManager();
+    private static SpawnManager instance = null;
+    private final Vector<Person> passivePersons = new Vector<>();
+    private SpawnAreaManager spawnAreaManager = SpawnAreaManager.getInstance();
+    private ConfigModel config = ConfigModel.getInstance();
+    private Random rnd = new Random();
+    private Vector<Person> persons = new Vector<>();
+    private PathManager pathManager = new PathManager();
 
 
-	private SpawnManager() {
-	}
+    private SpawnManager() {
+    }
 
-	public static SpawnManager getInstance() {
-		if (instance == null) instance = new SpawnManager();
-		return instance;
-	}
+    public static SpawnManager getInstance() {
+        if (instance == null) instance = new SpawnManager();
+        return instance;
+    }
 
-	public void createPersons() {
+    public void createPersons() {
         /*
-		 * spawn persons randomly or weighted
+         * spawn persons randomly or weighted
 		 */
-		try {
-			Class[] personTypes = {Class.forName("model.persons.YoungPerson"),
-					Class.forName("model.persons.MidAgePerson"),
-					Class.forName("model.persons.OldPerson"),
-					Class.forName("model.persons.HandicappedPerson")};
+        Class[] personTypes = {
+                YoungPerson.class, MidAgePerson.class, OldPerson.class, HandicappedPerson.class
+        };
 
-			if (!config.isWeighted()) {
-				for (int i = 0; i < this.config.getTotalPersons(); i++) {
-					int type = rnd.nextInt(personTypes.length);
-					this.spawnPerson(pathManager, personTypes[type]);
-				}
-			} else {
-				double personFactor = this.config.getTotalPersons() / 100;
-				int[] ageDistribution = new int[4];
-				ageDistribution[0] = (int) Math.round(config.getWeightedYoungPersons() * personFactor);
-				ageDistribution[1] = (int) Math.round(config.getWeightedMidAgePersons() * personFactor);
-				ageDistribution[2] = (int) Math.round(config.getWeightedOldPersons() * personFactor);
-				ageDistribution[3] = (int) Math.round(config.getWeightedHandicappedPersons() * personFactor);
-				if (ageDistribution[0] + ageDistribution[1] + ageDistribution[2] + ageDistribution[3] != config.getTotalPersons())
-					ageDistribution[3] = (int) config.getTotalPersons() - ageDistribution[0] - ageDistribution[1] - ageDistribution[2];
+        if (!config.isWeighted()) {
+            for (int i = 0; i < this.config.getTotalPersons(); i++) {
+                int type = rnd.nextInt(personTypes.length);
+                this.spawnPerson(pathManager, personTypes[type]);
+            }
+        } else {
+            double personFactor = this.config.getTotalPersons() / 100;
+            int[] ageDistribution = new int[4];
+            ageDistribution[0] = (int) Math.round(config.getWeightedYoungPersons() * personFactor);
+            ageDistribution[1] = (int) Math.round(config.getWeightedMidAgePersons() * personFactor);
+            ageDistribution[2] = (int) Math.round(config.getWeightedOldPersons() * personFactor);
+            ageDistribution[3] = (int) Math.round(config.getWeightedHandicappedPersons() * personFactor);
+            if (ageDistribution[0] + ageDistribution[1] + ageDistribution[2] + ageDistribution[3] != config.getTotalPersons())
+                ageDistribution[3] = (int) config.getTotalPersons() - ageDistribution[0] - ageDistribution[1] - ageDistribution[2];
 
-				for (int i = 0; i < ageDistribution.length; i++)
-					createPersons(personTypes[i], ageDistribution[i], pathManager);
-			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+            for (int i = 0; i < ageDistribution.length; i++)
+                createPersons(personTypes[i], ageDistribution[i], pathManager);
+        }
+    }
 
-	private void createPersons(Class klass, int count, PathManager pathManager) {
-		for (int i = 0; i < count; i++)
-			this.spawnPerson(pathManager, klass);
-	}
+    private void createPersons(Class<? extends Person> _class, int count, PathManager pathManager) {
+        for (int i = 0; i < count; i++)
+            this.spawnPerson(pathManager, _class);
+    }
 
-	/**
-	 * Generating different aged persons randomly
-	 * Created by suter1 on 05.10.2016
-	 */
-	private void spawnPerson(PathManager pathManager, Class<? extends Person> klass) {
-		Person newPerson;
-		try {
-			Class partypes[] = new Class[1];
-			partypes[0] = Position.class;
-			Constructor ct = klass.getConstructor(partypes);
-			SpawnArea spawnArea = spawnAreaManager.getSpawnAreas().get(ThreadLocalRandom.current().nextInt(0, spawnAreaManager.getSpawnAreas().size()));
-			newPerson = (Person) ct.newInstance(getSpawnPosition(spawnArea));
-			this.persons.add(newPerson);
-			Vertex nextHop = pathManager.getClosestVertex(newPerson.getCurrentPosition());
-			newPerson.setNextVertex(nextHop);
-			if (nextHop instanceof TargetVertex) newPerson.setTarget((TargetVertex) nextHop);
-			else newPerson.setTarget(nextHop.getShortestTarget());
-		} catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * Generating different aged persons randomly
+     * Created by suter1 on 05.10.2016
+     */
+    private void spawnPerson(PathManager pathManager, Class<? extends Person> _class) {
+        Person newPerson;
+        try {
+            Class partypes[] = new Class[1];
+            partypes[0] = Position.class;
+            Constructor ct = _class.getConstructor(partypes);
+            SpawnArea spawnArea = spawnAreaManager.getSpawnAreas().get(ThreadLocalRandom.current().nextInt(0, spawnAreaManager.getSpawnAreas().size()));
+            newPerson = (Person) ct.newInstance(getSpawnPosition(spawnArea));
+            this.persons.add(newPerson);
+            Vertex nextHop = pathManager.getClosestVertex(newPerson.getCurrentPosition());
+            newPerson.setNextVertex(nextHop);
+            if (nextHop instanceof TargetVertex) newPerson.setTarget((TargetVertex) nextHop);
+            else newPerson.setTarget(nextHop.getShortestTarget());
+        } catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * calculate possible spawn position in a selected SpawnArea
+    /**
+     * calculate possible spawn position in a selected SpawnArea
+     *
      * @param spawn spawnarea to give the boundaries where to place the start
+     *
      * @return Position
-	 */
-	private Position getSpawnPosition(SpawnArea spawn) {
-		Random r = new Random();
-		double x, y;
-		double randomWidth, randomHeight;
-		do {
-			randomWidth = (spawn.getBoundsInLocal().getWidth() - (config.getPersonRadius() * 2)) * r.nextDouble();
-			randomHeight = (spawn.getBoundsInLocal().getHeight() - (config.getPersonRadius() * 2)) * r.nextDouble();
-			x = spawn.getBoundsInLocal().getMinX() + randomWidth;
-			y = spawn.getBoundsInLocal().getMinY() + randomHeight;
-		}
-		while (!spawn.contains(x + config.getPersonRadius(), y + config.getPersonRadius())
-				|| !spawn.contains(x - config.getPersonRadius(), y - config.getPersonRadius())
-				|| !spawn.contains(x + config.getPersonRadius(), y - config.getPersonRadius())
-				|| !spawn.contains(x - config.getPersonRadius(), y + config.getPersonRadius()));
-		return new Position(x, y);
-	}
+     */
+    private Position getSpawnPosition(SpawnArea spawn) {
+        Random r = new Random();
+        double x, y;
+        double randomWidth, randomHeight;
+        do {
+            randomWidth = (spawn.getBoundsInLocal().getWidth() - (config.getPersonRadius() * 2)) * r.nextDouble();
+            randomHeight = (spawn.getBoundsInLocal().getHeight() - (config.getPersonRadius() * 2)) * r.nextDouble();
+            x = spawn.getBoundsInLocal().getMinX() + randomWidth;
+            y = spawn.getBoundsInLocal().getMinY() + randomHeight;
+        }
+        while (!spawn.contains(x + config.getPersonRadius(), y + config.getPersonRadius())
+                || !spawn.contains(x - config.getPersonRadius(), y - config.getPersonRadius())
+                || !spawn.contains(x + config.getPersonRadius(), y - config.getPersonRadius())
+                || !spawn.contains(x - config.getPersonRadius(), y + config.getPersonRadius()));
+        return new Position(x, y);
+    }
 
-	public Vector<Person> getPersons() {
-		return this.persons;
-	}
+    public Vector<Person> getPersons() {
+        return this.persons;
+    }
 
-	public PathManager getPathManager() {
-		return this.pathManager;
-	}
+    public PathManager getPathManager() {
+        return this.pathManager;
+    }
 
-	public void handlePersonsInTargetRange() {
-		Vector<Person> newPersons = new Vector<>();
-		for (Person p : this.persons)
-			if (p.isInGoal()) {
-				this.passivePersons.add(p);
-			} else
-				newPersons.add(p);
-		this.persons = newPersons;
-	}
+    public void handlePersonsInTargetRange() {
+        Vector<Person> newPersons = new Vector<>();
+        for (Person p : this.persons)
+            if (p.isInGoal()) {
+                this.passivePersons.add(p);
+            } else
+                newPersons.add(p);
+        this.persons = newPersons;
+    }
 
-	public void clear() {
-		this.passivePersons.clear();
-		this.persons.clear();
-		this.pathManager.clear();
-	}
+    public void clear() {
+        this.passivePersons.clear();
+        this.persons.clear();
+        this.pathManager.clear();
+    }
 
-	public Vector<Person> getPassivePersons() {
-		return this.passivePersons;
-	}
+    public Vector<Person> getPassivePersons() {
+        return this.passivePersons;
+    }
 }
